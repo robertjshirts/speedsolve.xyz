b911c7cf8335c8de1c383cdee15050ac3e3ef0c5
robertjshirts <333221.rob@gmail.com>
2024-09-30 23:06:55 -0600

    terraform setup, basic client

---
+++ client/components/Topbar.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="fixed top-0 z-50 w-full border-b bg-inherit backdrop-blur">
+    <div class="mx-auto flex max-w-screen-xl justify-between px-4 py-3">
+      <span>Speedsolve.xyz</span>
+      <span>Solo</span>
+      <span>Multiplayer</span>
+    </div>
+  </div>
+</template>
---
+++ client/layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <Topbar />
+  <div class="mx-auto flex max-w-screen-xl px-0 md:px-4">
+    <div class="pt-24 md:ps-72">
+      <slot />
+    </div>
+  </div>
+</template>
