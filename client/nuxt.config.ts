4dd86eacbe861679e2d03daac9ee2ebaf9cf0e39
robertjshirts <robertjshirts@gmail.com>
2024-10-02 18:34:47 -0600

    change nuxt config

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -2,10 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: [
-    '@nuxtjs/tailwindcss'
-  ],
+  modules: ['@nuxtjs/tailwindcss', '@nuxthub/core'],
   hub: {
-    database: true
+    database: true,
+    blob: true
   }
 })
