19f54ac5174f9964e607b66787729cc7ba0807df
robertjshirts <robertjshirts@gmail.com>
2024-10-09 13:07:00 -0600

    impl auth

---
+++ client/components/Login.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+import { useAuth0 } from '@auth0/auth0-vue';
+
+export default {
+  setup() {
+    const { loginWithRedirect } = useAuth0();
+
+    return {
+      login: () => {
+        loginWithRedirect();
+      }
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <button @click="login">Log in</button>
+  </div>
+</template>
