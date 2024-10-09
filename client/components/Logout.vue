446fa6b5959e22e6a89888c9d391a692f27a1e6c
robertjshirts <robertjshirts@gmail.com>
2024-10-09 13:07:00 -0600

    impl auth

---
+++ client/components/Logout.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import { useAuth0 } from '@auth0/auth0-vue';
+
+export default {
+  setup() {
+    const { logout } = useAuth0();
+
+    return {
+      logout: () => {
+        logout({ logoutParams: { returnTo: window.location.origin } });
+      }
+    }
+  }
+}
+</script>
