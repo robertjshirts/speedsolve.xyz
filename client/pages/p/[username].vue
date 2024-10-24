815d2b6b238f566f12af8e067bd7a42109ed3d3c
robertjshirts <robertjshirts@gmail.com>
2024-10-23 23:43:16 -0600

    update pfp size

--- client/pages/p/[username].vue
+++ client/pages/p/[username].vue
@@ -12,9 +12,9 @@ await getProfile(route.params.username as string);
     <div v-if="loading">Loading...</div>
     <div v-else-if="error">Error: {{ error.message }}</div>
     <div v-else-if="profile">
+      <img :src="profile.pfp" :alt="profile.username" class="max-w-20" />
       <p>Username: {{ profile.username }}</p>
       <p>Email: {{ profile.email }}</p>
-      <img :src="profile.pfp" :alt="profile.username" />
       <p>Member since: {{ new Date(profile.createdAt).toLocaleDateString() }}</p>
     </div>
   </div>
