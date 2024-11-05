fe81fbe82060663010812e8dad11a0713f3e9b88
robertjshirts <robertjshirts@gmail.com>
2024-11-05 16:22:20 -0700

    update scrambler

--- services/competition/scrambler.ts
+++ services/competition/scrambler.ts
@@ -3,14 +3,51 @@ export const generateScramble = (cube_type: CubeType) => {
 		cube_type === "3x3" ||
 		cube_type === "2x2"
 	) {
-		const moves = ["R", "L", "U", "D", "F", "B"];
-		const modifiers = ["", "'", "2"];
-		return Array.from({ length: 20 }, () => {
-			const move = moves[Math.floor(Math.random() * moves.length)];
-			const modifier =
-				modifiers[Math.floor(Math.random() * modifiers.length)];
-			return move + modifier;
-		}).join(" ");
+		return generateScrambleFor3x3();
 	}
 	return "R U R' U'";
+};
+
+const generateScrambleFor3x3 = () => {
+	const turnMoves = ['U', 'D', 'R', 'L', 'F', 'B'];
+
+    const hasDone: string[] = [];
+
+    const scramble: string[] = [];
+
+    for (let i = 0; i < 20; i += 1) {
+        let move = turnMoves[Math.floor(Math.random() * turnMoves.length)];
+        
+        let y = 0;
+            
+        while (hasDone.includes(move)) {
+            y++;
+            move = turnMoves[Math.floor(Math.random() * turnMoves.length)];
+        }
+
+        if (move == 'U' || move == 'D' || move == 'F') {
+            delete hasDone[hasDone.indexOf('R')];
+            delete hasDone[hasDone.indexOf('L')];
+        } else if (move == 'R' || move == 'L') {
+            delete hasDone[hasDone.indexOf('U')];
+            delete hasDone[hasDone.indexOf('D')];
+            delete hasDone[hasDone.indexOf('F')];
+        }
+
+        if (!hasDone.includes(move)) {
+            hasDone.push(move);
+        }
+
+        const isPrime = Math.floor(Math.random() * 2);
+        const is2 = Math.floor(Math.random() * 2);
+
+        if (isPrime)
+            move += '\'';
+        else if (is2)
+            move += '2';
+
+        scramble.push(move);
+    } 
+
+	return scramble.join(' ');
 };
