bb7f41d397ccc9e3ab262c7c4c4dca58ba3efac4
robertjshirts <robertjshirts@gmail.com>
2024-11-21 12:58:55 -0700

    switch to google stun

--- frontend/src/hooks/useWebRTC.ts
+++ frontend/src/hooks/useWebRTC.ts
@@ -19,28 +19,31 @@ export function useWebRTC(sendMessage: (type: MultiClientMessageType, payload?: 
     const pc = new RTCPeerConnection({
       iceServers: [
         {
-          urls: "stun:stun.relay.metered.ca:80",
-        },
-        {
-          urls: "turn:global.relay.metered.ca:80",
-          username: turnUsername,
-          credential: turnCredential,
-        },
-        {
-          urls: "turn:global.relay.metered.ca:80?transport=tcp",
-          username: turnUsername,
-          credential: turnCredential,
-        },
-        {
-          urls: "turn:global.relay.metered.ca:443",
-          username: turnUsername,
-          credential: turnCredential,
-        },
-        {
-          urls: "turns:global.relay.metered.ca:443?transport=tcp",
-          username: turnUsername,
-          credential: turnCredential,
-        },
+          urls: "stun:stun.l.google.com:19302",
+        }
+        // {
+        //   urls: "stun:stun.relay.metered.ca:80",
+        // },
+        // {
+        //   urls: "turn:global.relay.metered.ca:80",
+        //   username: turnUsername,
+        //   credential: turnCredential,
+        // },
+        // {
+        //   urls: "turn:global.relay.metered.ca:80?transport=tcp",
+        //   username: turnUsername,
+        //   credential: turnCredential,
+        // },
+        // {
+        //   urls: "turn:global.relay.metered.ca:443",
+        //   username: turnUsername,
+        //   credential: turnCredential,
+        // },
+        // {
+        //   urls: "turns:global.relay.metered.ca:443?transport=tcp",
+        //   username: turnUsername,
+        //   credential: turnCredential,
+        // },
       ], 
     });
 
